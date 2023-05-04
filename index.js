2ceb7d17a3602575070256811b461db85fbb7bf7
mehmetunlu <munlu4037@gmail.com>
2023-05-04 17:03:14 +0300

    Completed assingment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Your code here
+class Cat {
+    constructor(name) { this.name = name; }
+    speak() { return `${this.name} says meow!` }
+}
+
+class Dog {
+    constructor(name) { this.name = name; }
+    speak() { return `${this.name} says woof!` }
+}
+
+class Bird {
+    constructor(name, sex) { this.name = name; this.sex = sex };
+    speak() {
+        if (this.sex === "female") { return `It's me! ${this.name}, the parrot!`}
+        else if (this.sex === "male"){ return `${this.name} says squawk!` };
+    }
+}
+const bird = new Bird("Pablo", "male");
+const bird2 = new Bird("Mable", "female");
+
+
+console.log(bird.speak())
+console.log(bird2.speak())
